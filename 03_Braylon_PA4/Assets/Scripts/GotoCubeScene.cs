508a7b616d789fe65f7d5bb98a6fd73f2c525a5f
Hideyuki191 <hideyuki1000000@gmail.com>
2020-04-30 14:40:55 +0800

    Updated with MainScene

---
+++ 03_Braylon_PA4/Assets/Scripts/GotoCubeScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GotoCubeScene : MonoBehaviour
+{
+
+    private float Timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += 1 * Time.deltaTime;
+        
+        if(Timer > 1)
+        {
+            SceneManager.LoadScene("CubeScene");
+        }
+    }
+}
